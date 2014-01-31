ff0903dc3e77af3c95ac76f8267204d628373eaa
Ryan <ryan@Ryan-Morans-Macbook-Pro.local>
2014-01-31 09:59:29 -0500

    Initial Commit

---
+++ Quiz3/Quiz3AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz3AppDelegate.h
+//  Quiz3
+//
+//  Created by Ryan on 1/31/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz3AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz3/Quiz3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Quiz3ViewController.h
+//  Quiz3
+//
+//  Created by Ryan on 1/31/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz3ViewController : UIViewController
+
+@end
